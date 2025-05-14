31f944ed4eb721bc129166c1181d1cc2f3884eff
Richard <richarddeocampojr12@gmail.com>
2025-05-15 01:15:20 +0800

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()],
+    tailwindcss(),
+    VitePWA(
+        {
+          manifest: {
+            icons: [
+              {
+                "src": "public/Square150x150Logo.scale-400.png",
+                "sizes": "600x600",
+                type: "image/png",
+                purpose: "any maskable",
+              },
+            ]
+          }
+        }
+    ),
+  ],
   server: {
     middleware: [
       (req, res, next) => {
