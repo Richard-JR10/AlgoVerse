a75c5844e77b1ee7c4023b1caa83e163d8507c1c
Richard <richarddeocampojr12@gmail.com>
2025-05-17 14:27:18 +0800

    Updated PWA

--- vite.config.js
+++ vite.config.js
@@ -25,16 +25,21 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any"
+            "src": "/web-app-manifest-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/web-app-manifest-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
           }
         ]
       },
       workbox: {
         // Cache critical assets
-        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,gif,ico}', '512.png'],
+        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,gif,ico}'],
         runtimeCaching: [
           {
             // Handle navigation requests (e.g., page reloads)
