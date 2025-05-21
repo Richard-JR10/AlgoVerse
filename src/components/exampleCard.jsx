387f08b418aada226d5e891f5fda53af0d61fd13
Richard <richarddeocampojr12@gmail.com>
2025-05-21 20:24:16 +0800

    Responsive

--- src/components/exampleCard.jsx
+++ src/components/exampleCard.jsx
@@ -5,10 +5,10 @@ import ExampleView from "./exampleView.jsx";
 const ExampleCard = ({ cardInfo }) => {
     const { title, category, description, examples, id } = cardInfo;
     return (
-        <div className="card bg-base-100 max-w-90 min-w-90 min-h-60 max-h-60 shadow-lg">
+        <div className="card bg-base-100 max-w-90 min-h-60 max-h-60 shadow-lg">
             <div className="card-body p-5">
                 <div className="flex flex-row w-full justify-between mb-3.75">
-                    <h2 className="text-xl font-semibold">{title}</h2>
+                    <h2 className="text-xl font-semibold line-clamp-1">{title}</h2>
                     <div className="badge badge-accent rounded-full text-base-300">{category}</div>
                 </div>
 
